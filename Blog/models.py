44620a2b67e83d21075880f68c2088557c11bac5
FutureCodeBuilding <insideTheUnreal@gmail.com>
2019-02-10 20:36:19 +0700

    Добавили модели Processor & Configuration, миграцию пока не делали. Настроили URL для перехода в админку (?) Зарегистрировали модели Прописали переменные для в настройках BINARYDATA_ROOT / BINARYDATA_URL в Settings И ещё кое что, не помню уже =)

--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Processor(models.Model):
+	# модель обработок и отчётов
+    class Meta():
+	    verbose_name = ""
+	    verbose_name_plural = ""
+	
+    name = models.CharField("Наименование", max_length=100)
+    description = models.CharField("Описание", max_length=2000)
+    create = models.DateTimeField("Создан", auto_now_add=True)
+    tested = models.BooleanField("Протестировано")
+    binary = models.BinaryField("БинарныеДанные", upload_to="Processors/")
+
+    def __str__(self):
+        return self.name
